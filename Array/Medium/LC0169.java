c6fd262092c417ce88ac0373dba35c74c9ab1916
Pritesh Sorte <sortepritesh16@gmail.com>
2026-04-23 14:56:07 +0530

    LC169 Majority Element

---
+++ Array/Medium/LC0169.java
@@ -0,0 +1,21 @@
+package Array.Medium;
+
+public class LC0169{
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+        int count = 0;
+        int candidate = 0;
+
+        for(int num : nums){
+            if(count == 0){
+                candidate = num;
+            }
+            if(num == candidate){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        System.out.println(candidate);
+    }
+}
