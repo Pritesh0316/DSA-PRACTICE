c2725ccf2626b75bf85bd8110c64afbc54637284
Pritesh Sorte <sortepritesh16@gmail.com>
2026-04-26 00:18:57 +0530

    LC48 Rotate Image

---
+++ Array/Medium/LC0048.java
@@ -0,0 +1,35 @@
+package Array.Medium;
+
+public class LC0048 {
+    public static void main(String[] args) {
+        int[][] matrix = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for(int i=0; i<n; i++){
+            for(int j=i; j<m; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0; i<n; i++){
+            int start = 0;
+            int end = m-1;
+            while(start<=end){
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
