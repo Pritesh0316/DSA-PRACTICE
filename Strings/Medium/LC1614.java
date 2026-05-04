6c0247467eddd18e28c0be7fbd1204394fce34cb
Pritesh Sorte <sortepritesh16@gmail.com>
2026-05-05 01:53:00 +0530

    LC1614 Maximum Nesting Depth of the Parentheses

---
+++ Strings/Medium/LC1614.java
@@ -0,0 +1,20 @@
+package Strings.Medium;
+
+public abstract class LC1614 {
+    public static void main(String[] args) {
+        String s = "(1+(2*3)+((8)/4))+1";
+        int count = 0;
+        int depth = 0;
+
+        for(char c : s.toCharArray()){
+            if(c =='('){
+                count++;
+            }else if(c ==')'){
+                depth = Math.max(depth, count);
+                count--;
+            }
+        }
+
+        System.out.println(depth);
+    }
+}
