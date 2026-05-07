974b9a2ec43adb86a518b2ae6c02be28906e0664
Pritesh Sorte <sortepritesh16@gmail.com>
2026-05-08 01:21:08 +0530

    LC151 Reverse Words in a String

---
+++ Strings/Medium/LC0151.java
@@ -0,0 +1,19 @@
+package Strings.Medium;
+
+public class LC0151{
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        s = s.trim();
+        String[] words = s.split("\\s+");
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=words.length-1; i>=0; i--){
+            sb.append(words[i]);
+            if(i != 0){
+                sb.append(" ");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
