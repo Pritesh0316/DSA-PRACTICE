5e1ef0ed714c5b4ac7d78d21afd9b38ff8d2e292
Pritesh Sorte <sortepritesh16@gmail.com>
2026-05-09 07:33:36 +0530

    Middle of the Linked List

---
+++ LinkedList/Medium/LC0876.java
@@ -0,0 +1,32 @@
+package LinkedList.Medium;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class LC0876 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        System.out.println(slow.val);
+    }
+
+}
